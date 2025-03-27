adb02b410520ce8b521e1809048dd4398c356081
이호정 <freeface06@naver.com>
2025-03-27 14:18:41 +0900

    init

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,10 @@
 function saveFcmToken(token) {
     alert(token);
 }
+
+function sendToFlutter() {
+    const msg = "Flutter 안녕!";
+    if (window.FlutterChannel) {
+      window.FlutterChannel.postMessage(msg);
+    }
+  }
